10e52b10848622aba5fe02eb510ddce4624746ea
Jack Kerigan <support@kerigan.com>
2017-10-05 12:01:32 -0400

    working

--- wp-content/themes/kmaidx/modules/leads/AdminLeads.php
+++ wp-content/themes/kmaidx/modules/leads/AdminLeads.php
@@ -28,10 +28,10 @@ class AdminLeads
         $agentName  = $userMeta['first_name'][0] . ' ' . $userMeta['last_name'][0];
         $userData   = $this->getBuckets($agentName);
 
-        echo '<pre>',print_r($userData),'</pre>';
+        echo '<!--<pre>',print_r($userData),'</pre>-->';
         ?>
         <div class="wrap">
-            <h1 class="wp-heading-inline">Clients and Leads assigned to <?php echo $agentName; ?></h1>
+            <h1 class="wp-heading-inline" style="margin-bottom: 1rem;">Clients and Leads assigned to <?php echo $agentName; ?></h1>
 
             <table class="wp-list-table widefat fixed striped pages">
                 <thead>
@@ -39,28 +39,32 @@ class AdminLeads
                     <th scope="col" id="title" class="manage-column column-name column-primary sortable desc"><a href="?page=bb-admin&amp;orderby=name&amp;order=asc"><span>Name</span><span class="sorting-indicator"></span></a></th>
                     <th scope="col" id="phone" class="manage-column column-phone"><span>Phone Number</span></th>
                     <th scope="col" id="email" class="manage-column column-email"><span>Email Address</span></th>
-                    <th scope="col" id="properties" class="manage-column column-properties"><span>Saved Properties</span></th>
+                    <th scope="col" id="email" class="manage-column column-address"><span>Physical Address</span></th>
                 </tr>
                 </thead>
                 <tbody>
-                <tr>
-                    <td><strong>Ted</strong></td>
-                    <td><strong>123-123-1234</strong></td>
-                    <td><strong>user@domain.com</strong></td>
-                    <td><strong><?php echo count($mlsNumbers); ?></strong></td>
-                </tr>
-                <tr>
-                    <td colspan="4">
-                        <p>Saved Properties:</p>
-                        <ul>
-                            <?php
-                            foreach ($mlsNumbers as $mlsNumber) {
-                                echo '<li><a href="/listing/?mls=' . $mlsNumber . '" target="_blank">' . $mlsNumber . '</a></li>';
-                            }
-                            ?>
-                        </ul>
-                    </td>
-                </tr>
+                <?php
+                foreach ($userData as $user) { ?>
+                    <tr>
+                        <td><strong><?php echo $user['first_name'][0] . ' ' . $user['last_name'][0]; ?></strong></td>
+                        <td><strong><a href="tel:<?php echo $user['phone1'][0]; ?>" ><?php echo $user['phone1'][0]; ?></a></strong></td>
+                        <td><strong><a href="mailto:<?php echo $user['email']; ?>" ><?php echo $user['email']; ?></a></strong></td>
+                        <td><strong><?php echo $user['addr1'][0]; echo $user['addr2'] != '' ? ', ' . $user['addr2'] : ''; ?><br>
+                                <?php echo $user['city'][0] . ', ' . $user['thestate'][0] . $user['zip'][0]; ?></strong></td>
+                    </tr>
+                    <tr>
+                        <td colspan="4">
+                            <p>Saved Properties ( <?php echo count($user['buckets']); ?> ):</p>
+                            <ul>
+                                <?php
+                                foreach ($user['buckets'] as $mlsNumber) {
+                                    echo '<li><a href="/listing/?mls=' . $mlsNumber . '" target="_blank">' . $mlsNumber . '</a></li>';
+                                }
+                                ?>
+                            </ul>
+                        </td>
+                    </tr>
+                <?php } ?>
                 </tbody>
             </table>
 
