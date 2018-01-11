f8f748d2035ec6245832598214bd731ce6b9abff
Bryan Baird <bryan@kerigan.com>
2018-01-11 12:15:15 -0500

    add open houses to listing page

---
+++ wp-content/themes/kmaidx/template-parts/listing-open-houses.php
@@ -0,0 +1,15 @@
+<?php
+?>
+<h2>Upcoming Open Houses</h2>
+<div class="card-columns">
+    <?php foreach($openHouses as $openHouse){
+        $addressString = urlencode($openHouse->street_address.' '.$openHouse->city.', '.$openHouse->state); ?>
+        <div class="card" style="border-bottom:1px solid #ddd;">
+            <div class="card-block">
+                <p class="card-text"><strong>Date:</strong> <?php echo date('M j, Y', strtotime($openHouse->event_start)); ?><br>
+                <strong>Time:</strong> <?php echo date('h:s a', strtotime($openHouse->event_start)); ?> - <?php echo date('h:s a', strtotime($openHouse->event_end)); ?></p>
+                <a href="https://www.google.com/maps/place/<?php echo $addressString; ?>" class="card-link">Get Directions</a>
+            </div>
+        </div>
+    <?php } ?>
+</div>
