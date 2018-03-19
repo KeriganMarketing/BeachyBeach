f2b4c344feab37c30395a5042f138b109ef65de3
programmer-man <bryan@kerigan.com>
2018-03-19 17:09:47 -0400

    merge fix

--- wp-content/themes/kmaidx/template-parts/mls-commerciallisting.php
+++ wp-content/themes/kmaidx/template-parts/mls-commerciallisting.php
@@ -45,7 +45,7 @@ foreach ($results as $result) { ?>
             <div class="tile-info">
 
                 <div class="tile-section">
-                    <span class="addr1"><?php echo $result->street_number . ' ' . $result->street_name .' '. $listingInfo->street_suffix; ?></span>
+                    <span class="addr1"><?php echo $result->street_number . ' ' . $result->street_name .' '. $result->street_suffix; ?></span>
 					<?php if ( $result->unit_number != '' ) { ?>
                         <span class="unit"><?php echo $result->unit_number; ?></span><?php } ?>
                     <br><span class="city"><?php echo $result->city; ?></span>,
